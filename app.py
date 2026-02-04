bd2ae263bcbfc6115d8a70841d989c685526ed44
Ahnaf Akif <26aakif@cpsd.is>
2026-02-04 15:54:51 -0500

    EWFWEFW

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, url_for
+import firewall
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    exam_status = firewall.exam_status()
+    devices = firewall.connected_devices()
+    return render_template("index.html",
+                           exam_status=exam_status,
+                           devices=devices)
+
+@app.route("/exam/on")
+def exam_on():
+    firewall.exam_on()
+    return redirect(url_for("index"))
+
+@app.route("/exam/off")
+def exam_off():
+    firewall.exam_off()
+    return redirect(url_for("index"))
+
+@app.route("/device/block/<mac>")
+def block_device(mac):
+    firewall.block_device(mac)
+    return redirect(url_for("index"))
+
+@app.route("/device/unblock/<mac>")
+def unblock_device(mac):
+    firewall.unblock_device(mac)
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

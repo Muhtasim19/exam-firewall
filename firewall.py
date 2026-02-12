899158bb634452af6309832108d185d6b579f291
Ahnaf Akif <26aakif@cpsd.us>
2026-02-12 19:19:51 +0000

    Add hostname column using dnsmasq leass

--- firewall.py
+++ firewall.py
@@ -16,8 +16,24 @@ def exam_status():
     )
     return result.stdout.strip()
 
+
 def connected_devices():
     devices = []
+    leases = {}
+
+    try:
+        with open("/var/lib/misc/dnsmasq.leases", "r") as f:
+            for line in f.readlines():
+                parts = line.split()
+                if len(parts) >= 4:
+                    mac = parts[1]
+                    ip = parts[2]
+                    hostname = parts[3]
+                    leases[ip] = hostname
+    except:
+        pass
+
+
     output = subprocess.check_output("ip neigh", shell=True, text=True)
 
     for line in output.splitlines():
@@ -27,10 +43,15 @@ def connected_devices():
             ip = parts[0]
             mac = parts[4]
             state = parts[-1]
+
+            
             if ip.startswith("192.168.50."):
+                hostname = leases.get(ip, "Unknown")
+
                 devices.append({
                     "ip": ip,
                     "mac": mac,
+                    "hostname": hostname,
                     "state": state
                 })
 

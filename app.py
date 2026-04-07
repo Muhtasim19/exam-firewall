d0f74a4f690a013310a165227f8953b7ab0f8511
Muhtasim <162151530+Muhtasim19@users.noreply.github.com>
2026-04-07 13:19:06 -0400

    Remove localhost restriction from run command

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def refresh_devices():
     return redirect(url_for("index"))
 
 # =========================
-# RUN (LOCALHOST ONLY)
+# RUN
 # =========================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
